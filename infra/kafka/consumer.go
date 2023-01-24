44d02aa4863af70eeeca54e404702ad51b2e8854
Willian Ribeiro <willian.ribeiro@eemovel.com>
2023-01-24 15:08:44 -0300

    feat: add kafka consumer

--- infra/kafka/consumer.go
+++ infra/kafka/consumer.go
@@ -1,6 +1,7 @@
 package kafka
 
 import (
+	"fmt"
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
 	"log"
 	"os"
@@ -18,5 +19,17 @@ func (l *KafkaConsumer) Consume() {
 	c, err := ckafka.NewConsumer(configMap)
 	if err != nil {
 		log.Fatalf("error consuming kafka message")
+	}
+	topics := []string{os.Getenv("KafkaReadTopic")}
+
+	c.SubscribeTopics(topics, nil)
+
+	fmt.Print("Kafka consumer has been started")
+
+	for {
+		msg, err := c.ReadMessage(-1)
+		if err == nil {
+			k.MsgChanel <- msg
+		}
 	}
 }
